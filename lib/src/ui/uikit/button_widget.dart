0bab86ef73578e83ffa59598fe82819c9327f907
Akrid <Akrid782@mail.ru>
2021-12-02 09:42:32 +0300

    Добавил цвет кнопки и доработал виджет кнопки

--- lib/src/ui/uikit/button_widget.dart
+++ lib/src/ui/uikit/button_widget.dart
@@ -1,30 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:where_to_go_today/src/ui/res/colors/colors.dart';
 
-/// Виджет, отображающий прогресс
-/// Требуется передать  в конструкторе вещественное число
-/// Если прогресс меньше единицы - отображается кружок с количеством процентов
-/// Если прогресс равен единице - показывается иконка с галочкой
+/// Виджет, кнопки
+/// Требуется передать  в конструкторе заголовок кнопки и callBack функцию
+/// Так же есть необязательные парамертры как process для отображения прелоадера
+/// и флаг disable блокирование кнопки
 class ButtonWidget extends StatelessWidget {
+  final String title;
+  final bool process;
+  final bool disable;
+  final void Function()? callBackPressed;
+
   const ButtonWidget({
     Key? key,
     required this.title,
     required this.callBackPressed,
+    this.process = false,
+    this.disable = false,
   }) : super(key: key);
-
-  final String title;
-  final void Function()? callBackPressed;
 
   @override
   Widget build(BuildContext context) {
+    final Color textColor = (disable
+        ? ProjectColors.textColorDisabled
+        : ProjectColors.textColorPrimary);
+
     return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        primary: const Color.fromRGBO(235, 206, 12, 1),
+      style: ButtonStyle(
+        padding: MaterialStateProperty.all<EdgeInsets>(
+          const EdgeInsets.all(8),
+        ),
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(12.0)),
+            side: BorderSide(color: ProjectColors.borderColorButton),
+          ),
+        ),
+        backgroundColor: MaterialStateProperty.resolveWith<Color>(
+          (Set<MaterialState> states) {
+            return ProjectColors.buttonColor;
+          },
+        ),
       ),
-      onPressed: callBackPressed,
-      child: Text(
-        title,
-        style: const TextStyle(color: Color.fromRGBO(0, 0, 0, 1)),
-      ),
+      onPressed: disable ? null : callBackPressed,
+      child: !process
+          ? Text(
+              title,
+              style: TextStyle(
+                color: textColor,
+                fontSize: 16,
+              ),
+            )
+          : const CircularProgressIndicator(
+              valueColor:
+                  AlwaysStoppedAnimation<Color>(ProjectColors.preloaderColor),
+            ),
     );
   }
 }
