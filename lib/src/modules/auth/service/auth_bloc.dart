0652470f45f750bd040d4f37a2969f7d8607a914
Nikolay <n.ivanov@spb.mcart.ru>
2021-12-21 08:39:22 +0300

    Добваил состояния в AuthBloc

--- lib/src/modules/auth/service/auth_bloc.dart
+++ lib/src/modules/auth/service/auth_bloc.dart
@@ -7,18 +7,53 @@ class AuthBloc extends Bloc<AuthEvent, AuthState>
     with CanThrowExceptionBlocMixin {
   AuthBloc() : super(LoadingState()) {
     // ignore: no-empty-block
-    on<SendSmsCodeEvent>((event, emit) {});
+    on<SendSmsCodeEvent>((event, emit) {
+      emit(LoadingState());
+
+      // ignore: todo
+      //TODO:реализация метода по отправки смс
+
+      emit(SuccessState());
+    });
 
     // ignore: no-empty-block
-    on<LoginBySsoEvent>((event, emit) {});
+    on<LoginBySsoEvent>((event, emit) {
+      emit(LoadingState());
+
+      // ignore: todo
+      //TODO:реализация метода авторизации через соц сеть
+
+      emit(SuccessState());
+    });
 
     // ignore: no-empty-block
-    on<LoginByPhoneEvent>((event, emit) {});
+    on<LoginByPhoneEvent>((event, emit) {
+      emit(LoadingState());
+
+      // ignore: todo
+      //TODO:реализация метода авторизации через телефон
+
+      emit(SuccessState());
+    });
 
     // ignore: no-empty-block
-    on<LogoutEvent>((event, emit) {});
+    on<LogoutEvent>((event, emit) {
+      emit(LoadingState());
+
+      // ignore: todo
+      //TODO:реализация метода выхода
+
+      emit(SuccessState());
+    });
 
     // ignore: no-empty-block
-    on<RegistrationEvent>((event, emit) {});
+    on<RegistrationEvent>((event, emit) {
+      emit(LoadingState());
+
+      // ignore: todo
+      //TODO:реализация метода регистрации
+
+      emit(SuccessState());
+    });
   }
 }
