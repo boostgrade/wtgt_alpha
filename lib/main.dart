a200c0106e75547a2a5c398de957783d6ec13725
Nikolay <n.ivanov@spb.mcart.ru>
2022-02-01 00:18:12 +0300

    Инициализация firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:where_to_go_today/src/app/ui/app_store.dart';
 import 'package:where_to_go_today/src/di/app_dependency.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'firebase_options.dart';
 import 'src/app/ui/app.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   final globalDeps = AppDependencies();
   await globalDeps.init();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(
     Provider(
